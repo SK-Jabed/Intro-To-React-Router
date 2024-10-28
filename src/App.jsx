3546bbc921183aedf21f91c52872fe96d745067d
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-28 13:14:01 +0600

    Intro to React Router Repository Added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import DaisyNavbar from './components/DaisyNavbar/DaisyNavbar'
+
+function App() {
+
+  return (
+    <>
+      
+
+      <NavBar></NavBar>
+      <DaisyNavbar></DaisyNavbar>
+      <h1 className='text-xl rounded-full text-rose-500'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
